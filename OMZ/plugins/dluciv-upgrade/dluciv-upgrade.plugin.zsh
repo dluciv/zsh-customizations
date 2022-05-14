e337bb5227958de8d1ae61ae0982fc6c8daf2604
Dmitry V. Luciv <dluciv@dluciv.name>
2022-05-14 19:04:18 +0300

    Self-update customizations

--- OMZ/plugins/dluciv-upgrade/dluciv-upgrade.plugin.zsh
+++ OMZ/plugins/dluciv-upgrade/dluciv-upgrade.plugin.zsh
@@ -1,13 +1,33 @@
+function _upd_dzcust () {
+  local _updpl_curpath=${(%):-%x}
+  pushd ${_updpl_curpath:A:h} >/dev/null
+  echo "Updading ZSH customizations in $(pwd)"
+  local _rv=0
+  if git remote &>/dev/null; then
+    git stash &>/dev/null
+    git pull
+    _rv=$?
+    git stash pop &>/dev/null
+  else
+    _rv=$?
+    echo "ZSH customizations are likely not in Git with remotes"
+  fi
+  popd >/dev/null
+  return $_rv
+}
+
 case $OSTYPE in
   linux-android*)
     function upgrade () {
       pkg upgrade
+      _upd_dzcust
       omz update
     }
   ;;
   darwin*)
     function upgrade () {
       brew upgrade --greedy
+      _upd_dzcust
       omz update
     }
   ;;
@@ -15,12 +35,15 @@ case $OSTYPE in
     function upgrade () {
       apt-cyg upgrade-self
       apt-cyg dist-upgrade
+      _upd_dzcust
+      omz update
     }
   ;;
   *) # linux-gnu* and others with upgrade defined
     if [[ -n "${aliases[upgrade]}" ]]; then
       function _dluciv_upgrade () {
         upgrade
+        _upd_dzcust
         omz update
       }
       # upgrade alias already substituted to function, so...
@@ -31,11 +54,13 @@ case $OSTYPE in
       functions[_omz_plug_upgrade]=${functions[upgrade]}
       function upgrade () {
         _omz_plug_upgrade
+        _upd_dzcust
         omz update
       }
     else
-      echo "Use plugin defining some upgrade method before. Only upgrading OMZ..."
+      echo "Use plugin defining some upgrade method before. Only upgrading OMZ and customizations..."
       function upgrade () {
+        _upd_dzcust
         omz update
       }
     fi

1f38f1dd8669861c683ea809a240abaec95cc6ad
Dmitry V. Luciv <dluciv@dluciv.name>
2022-12-15 12:30:07 +0300

    Of course sudo! =)

--- OMZ/plugins/dluciv-voidlinux/dluciv-voidlinux.plugin.zsh
+++ OMZ/plugins/dluciv-voidlinux/dluciv-voidlinux.plugin.zsh
@@ -5,7 +5,7 @@
 # XBPS - https://docs.voidlinux.org/xbps/index.html
 
 if which xbps-install &>/dev/null; then
-  alias upgrade='xbps-install --sync --update'
+  alias upgrade='sudo xbps-install --sync --update'
 else
   >&2 echo "Void Linux upgrade plugin cannot be used with on this distribution"
 fi

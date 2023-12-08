173f742a0a85f0f71cece8328985aa043f723dfb
Dmitry V. Luciv <dluciv@dluciv.name>
2023-12-08 12:51:10 +0300

    MSYS scoop / choco

--- OMZ/plugins/dluciv-msys/dluciv-msys.plugin.zsh
+++ OMZ/plugins/dluciv-msys/dluciv-msys.plugin.zsh
@@ -3,6 +3,10 @@ if [[ $OSTYPE == msys* ]]; then
   function upgrade () {
     pacman -Syyu
     echo -e 'y\ny' | /usr/bin/pacman -Scc
+    if which _windows-pms-upgrade &> /dev/null; then
+      echo "Upgrading Windows package managers"
+      _windows-pms-upgrade
+    fi
   }
 
 else

14521fb2321e8165b79eb9c725d361400a317b29
Dmitry V. Luciv <dluciv@dluciv.name>
2024-04-02 18:07:45 +0300

    And also cleanup

--- OMZ/plugins/dluciv-windows/dluciv-windows.plugin.zsh
+++ OMZ/plugins/dluciv-windows/dluciv-windows.plugin.zsh
@@ -2,6 +2,7 @@ function _scoop-update () {
   scoop update
   if true; then
     scoop update -a -f
+    scoop cleanup -a -k
   else
     # older
     for app in $(ls -1 $USERPROFILE/scoop/apps); do

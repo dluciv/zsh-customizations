f4e02ea47b861aa623c972182f890c7449652817
Dmitry V. Luciv <dluciv@dluciv.name>
2024-08-22 16:02:09 +0300

    Конечно же sudo

--- OMZ/plugins/dluciv-ubuntu/dluciv-ubuntu.plugin.zsh
+++ OMZ/plugins/dluciv-ubuntu/dluciv-ubuntu.plugin.zsh
@@ -1,8 +1,8 @@
 if [[ $OSTYPE == linux-gnu && $(lsb_release --id --short | tail -n1) == Ubuntu ]]; then
   upgrade() {
-    apt upgrade
-    apt autoclean
-    apt autoremove
+    sudo apt upgrade
+    sudo apt autoclean
+    sudo apt autoremove
   }
 else
   >&2 echo "Ubuntu upgrade plugin cannot be used with '$OSTYPE' OS"

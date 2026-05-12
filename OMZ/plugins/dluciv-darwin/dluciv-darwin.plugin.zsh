c6cf2914d9fe4acf22fd76fa02ea0c2f9b417e0e
Dmitry V. Luciv <dluciv@dluciv.name>
2026-05-12 14:22:11 +0300

    brews_upgrade

--- OMZ/plugins/dluciv-darwin/dluciv-darwin.plugin.zsh
+++ OMZ/plugins/dluciv-darwin/dluciv-darwin.plugin.zsh
@@ -1,13 +1,17 @@
 if [[ $OSTYPE == darwin* ]]; then
-  brews_upgrade() {
-    if command -v zb &>/dev/null; then
-      zb update
-    fi
-    if command -v brew &>/dev/null; then
-      brew upgrade --greedy
-    fi
-    alias upgrade='brews_upgrade'
-  }
+    brews_upgrade() {
+	local outdated_names
+	if command -v zb &>/dev/null; then
+	    outdated_names=$(zb outdated --json | jq -r '.[] | .name')
+	    while IFS= read -r outdated_name; do
+		zb install $outdated_name
+	    done <<< "$outdated_names"
+	fi
+	if command -v brew &>/dev/null; then
+	    brew upgrade --greedy
+	fi
+	alias upgrade='brews_upgrade'
+    }
 else
-  >&2 echo "Darwin upgrade plugin cannot be used with '$OSTYPE' OS"
+    >&2 echo "Darwin upgrade plugin cannot be used with '$OSTYPE' OS"
 fi
